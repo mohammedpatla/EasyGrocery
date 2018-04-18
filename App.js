47d6f44ff2484033720c8dcc00a4cce9bc4c192b
dervinis <ddrdeivis@gmail.com>
2018-04-18 10:20:31 -0400

    Added Just the test Code

--- App.js
+++ App.js
@@ -1,22 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default class App extends React.Component {
+  _onPressButton() {
+    Alert.alert("You tapped the button!")
+  }
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.body}>
+
+        <View style={styles.buttonContainer}>
+          <Button style={styles.button_Inside}
+            onPress={this._onPressButton}
+            title="Press ME"
+          />
+        </View>
+
         <Text>Open up App.js to start working on your app!</Text>
-        <Text>Finally it fucking works</Text>
+        <Text>Boom Boom Boom</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
+
+    justifyContent: 'space-between',
+    backgroundColor: '#4286f4',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  buttonContainer: {
+    marginTop: 50,
+    backgroundColor: '#6ba0f4',
+  },
+
+  button_Inside: {
+    color: 'red',
+  }
 });
